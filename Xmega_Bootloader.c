88108ab8025de9dd88e0c2d25bbaef845495d253
prenticedavid <david.prentice@farming.co.uk>
2022-03-29 17:19:39 +0100

    cast to ADDR_T comment

--- Xmega_Bootloader.c
+++ Xmega_Bootloader.c
@@ -72,7 +72,7 @@ int main(void)
                 sendchar(RESPONSE_YES);                       // Yes, we do autoincrement.
 
             else if(val == COMMAND_SET_ADDRESS) {             // Set address (words, not bytes)
-                address = ((ADDR_T)recchar() << 8) | recchar();
+                address = ((ADDR_T)recchar() << 8) | recchar(); // ensure unsigned ADDR_T expression
                 sendchar(RESPONSE_OKAY);                      // Send OK back.
             }
 
